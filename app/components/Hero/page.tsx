99b5becc42cfb94cf04863429e54dfa73a6f760e
muneeb <muneebusmani8355@gmail.com>
2025-04-29 00:23:39 +0500

    some more changes to hero section

--- app/components/Hero/page.tsx
+++ app/components/Hero/page.tsx
@@ -1,10 +1,10 @@
 import AnimatedBackground from "./AnimatedBackground";
 const Hero = () => (
-  <section id="hero" className="relative w-full h-[125vh]  -mt-16 pt-16 ">
+  <section id="hero" className="relative w-full -mt-16 pt-16 ">
     <div className="absolute inset-0 flex items-center justify-center text-center p-4 z-10">
       <div className="space-y-6 max-w-4xl mx-auto">
-        <h1 className="text-4xl md:text-6xl font-bold text-white animate-fade-in [animation-delay:200ms] opacity-0 translate-y-10">
-          <span className="bg-gradient-to-r from-red-500 via-orange-500 to-yellow-400   bg-clip-text text-transparent">
+        <h1 className="text-4xl md:text-6xl font-bold text-white animate-fade-in [animation-delay:200ms] opacity-0 translate-y-10 text-shadow-lg text-shadow-pink-50/50  drop-shadow-2xl drop-shadow-rose-700">
+          <span className="text-black/75 bg-clip-text">
             Your trusted website development agency
           </span>
         </h1>
